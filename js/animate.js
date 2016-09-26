53d8b60f69a18352bba197bc4fb74b2a42e9be14
Cam Crews <camcrews@gmail.com>
2016-09-26 15:41:36 -0400

    Added animation for first load only

--- js/animate.js
+++ js/animate.js
@@ -1,30 +1,37 @@
 document.addEventListener("DOMContentLoaded",function(){
-
+  if( !Cookies.get("animate") ) {
+    goRight();
+    Cookies.set("animate",1);
+  }
+  
 });
 
 function goRight() {
-  var i = 0;
-  var animationId = setInterval(function(){
-    // animate...
-    document.querySelector("#circle").style.marginLeft += "5px";
-    // get out after 25 steps
-    if( i > 25 ) {
-      clearInterval(animationId);
-      goLeft();
-    }
-    i++; // increase counter
-  },100);
+  $("#circle").animate({
+    marginLeft: "800px"
+  },3000,function(){
+    goLeft();
+  });
 }
 function goLeft() {
-  var i = 0;
-  var animationId = setInterval(function(){
-    // animate...
-    document.querySelector("#circle").style.marginLeft -= "5px";
-    // get out after 25 steps
-    if( i > 25 ) {
-      clearInterval(animationId);
-      goRight();
-    }
-    i++; // increase counter
-  },100);
+  $("#circle").animate({
+    marginLeft: "0px"
+  },3000,function(){
+    goRight();
+  });
 }
+
+// failed vanilla js animation
+// marginLeft blank???
+
+  // var i = 0;
+  // var animationId = setInterval(function(){
+  //   // animate...
+  //   console.log(i, document.getElementById("circle").style.marginLeft);
+  //   document.getElementById("circle").style.marginLeft = Math.max(0,parseInt(document.getElementById("circle").style.marginLeft))+10+"px";
+  //   // get out after 25 steps
+  //   if( i > 25 ) {
+  //     clearInterval(animationId);
+  //   }
+  //   i++; // increase counter
+  // },100);
